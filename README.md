766c7f08e217ddb1bce49d6f55dd615a743da2d4
Martin Zimdahl <martinzimdahl@outlook.com>
2019-06-11 21:21:53 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# androidish_ink_well
+
+An inkWell that splashes like Android.
+
+## Getting Started
+
+This project contains a modified InkWell called AndroidishInkWell and a modified InkSplash called AndroidishInkSplash.
+
+Add "AndroidishInkSplash.splashFactory" to ThemeData.
+
+Parameter "coverWholeRadius" of AndroidishInkWell if true sets the splash to 50% of the radius.
+
+Please see example file.
---
+++ example/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:androidish_ink_well/androidish_ink_splash.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:androidish_ink_well/androidish_ink_well.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Androidish InkWell'),
+        ),
+        body: Theme(
+          data: Theme.of(context).copyWith(splashFactory: AndroidishInkSplash.splashFactory),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Material(
+                  color: Colors.grey.shade800,
+                  child: AndroidishInkWell(
+                    child: MaterialButton(
+                      elevation: 12,
+                      child: Text(
+                        'SPLASH WHOLE RADIUS',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 48, vertical: 24),
+                    ),
+                    onTap: () => Scaffold.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('See that splash!'),
+                            action: SnackBarAction(
+                              label: 'WOW',
+                              onPressed: () => Scaffold.of(context).hideCurrentSnackBar(),
+                            ),
+                          ),
+                        ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 48),
+              Center(
+                child: Material(
+                  color: Colors.grey.shade800,
+                  child: AndroidishInkWell(
+                    coverWholeRadius: false,
+                    child: MaterialButton(
+                      elevation: 12,
+                      child: Text(
+                        'SPLASH 50% OF RADIUS',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 48, vertical: 24),
+                    ),
+                    onTap: () => Scaffold.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('See that splash!'),
+                            action: SnackBarAction(
+                              label: 'WOW',
+                              onPressed: () => Scaffold.of(context).hideCurrentSnackBar(),
+                            ),
+                          ),
+                        ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
